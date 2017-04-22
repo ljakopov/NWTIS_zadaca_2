24f02400a0673132736fcf121e7daf314ce8b823
Lovro <ljakopov@foi.hr>
2017-04-23 01:23:21 +0200

    Slanje poruke i prvi dio obrade

--- src/java/org/foi/nwtis/ljakopov/web/dretve/ObradaPoruka.java
+++ src/java/org/foi/nwtis/ljakopov/web/dretve/ObradaPoruka.java
@@ -38,6 +38,10 @@ public class ObradaPoruka extends Thread {
         String port = konf.dajPostavku("mail.port");
         String korisnik = konf.dajPostavku("mail.usernameThread");
         String lozinka = konf.dajPostavku("mail.passwordThread");
+        String odredjenePoruke = konf.dajPostavku("mail.folderNWTiS");
+        String ostalePoruke = konf.dajPostavku("mail.folderOther");
+        String nwtis_porukaString = konf.dajPostavku("mail.subject");
+
         int trajanjeCiklusa = Integer.parseInt(konf.dajPostavku("mail.timeSecThread"));
         //TODO i za ostale parametre
         int trajanjeObrade = 0;
@@ -55,18 +59,44 @@ public class ObradaPoruka extends Thread {
                 Store store = session.getStore("imap");
                 store.connect(server, korisnik, lozinka);
 
+                /**
+                 * kreiraju se novi folderi za spremanje poruka koje zadovoljavaju regex i 
+                 * ostalih poruka
+                 */
+                Folder folderZaSpremanje = store.getDefaultFolder();
+                if (folderZaSpremanje.getFolder(ostalePoruke).exists() != false) {
+                    folderZaSpremanje.getFolder(ostalePoruke).create(Folder.HOLDS_MESSAGES);
+                }
+
+                if (folderZaSpremanje.getFolder(odredjenePoruke).exists() != false) {
+                    folderZaSpremanje.getFolder(odredjenePoruke).create(Folder.HOLDS_MESSAGES);
+                }
+
                 // Open the INBOX folder
                 Folder folder = store.getFolder("INBOX");
                 folder.open(Folder.READ_ONLY);
-                
+                Folder folderZaSpremanjeIspravnihPoruka = store.getFolder(odredjenePoruke);
+                folderZaSpremanjeIspravnihPoruka.open(Folder.READ_ONLY);
+                Folder folderZaSpremanjeOstalihPoruka = store.getFolder(ostalePoruke);
+                folderZaSpremanjeOstalihPoruka.open(Folder.READ_ONLY);
+
+                String subjekt="";
+                String porukaString="";
                 Message[] messages = folder.getMessages();
-                for(int i = 0; i<messages.length; ++i){
+                System.out.println("OVo je broj porula: " + messages.length);
+                for (int i = 0; i < messages.length; ++i) {
+                    System.out.println("OVo je SUBJECT: " + messages[i].getSubject());
+                    if(messages[i].getSubject().equals(nwtis_porukaString)){
+                        //folder.copyMessages((Message[])messages[i].to, folder);
+                        
+                    }
+                    //subjekt=messages[i].getSubject();
                     //TODO dovrišiti čitanje, obradu i prebacivanje u mape
                 }
                 redniBrojCiklusa++;
                 System.out.println("Obrada prouka u cilkusu: " + redniBrojCiklusa);
                 sleep(trajanjeCiklusa * 1000 - trajanjeObrade);
-                
+
             } catch (NoSuchProviderException ex) {
                 Logger.getLogger(ObradaPoruka.class.getName()).log(Level.SEVERE, null, ex);
             } catch (MessagingException | InterruptedException ex) {
--- src/java/org/foi/nwtis/ljakopov/web/zrna/SlanjePoruke.java
+++ src/java/org/foi/nwtis/ljakopov/web/zrna/SlanjePoruke.java
@@ -5,18 +5,30 @@
  */
 package org.foi.nwtis.ljakopov.web.zrna;
 
+import static java.lang.ProcessBuilder.Redirect.to;
+import static java.util.Date.from;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.SendFailedException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import static javax.ws.rs.client.Entity.text;
 
 /**
  *
- * @author Lovro
+ * @author ljakopov
  */
 @Named(value = "slanjePoruke")
 @RequestScoped
 public class SlanjePoruke {
 
-    private String posluzitelj;
+    private String posluzitelj = "127.0.0.1";
     private String salje;
     private String prima;
     private String predmet;
@@ -62,9 +74,46 @@ public class SlanjePoruke {
 
     public String saljiPoruku() {
         //TODO dodaj za slanje poruke prema primjeru s predavanja koji je priložen uz zadaću
-        this.sadrzaj = "";
-        this.predmet = "";
-        this.prima = "";
+
+        String status;
+
+        try {
+            // Create the JavaMail session
+            java.util.Properties properties = System.getProperties();
+            properties.put("mail.smtp.host", posluzitelj);
+
+            Session session
+                    = Session.getInstance(properties, null);
+
+            // Construct the message
+            Message message = new MimeMessage(session);
+
+            // Set the from address
+            Address fromAddress = new InternetAddress(this.salje);
+            message.setFrom(fromAddress);
+
+            // Parse and set the recipient addresses
+            Address[] toAddresses = InternetAddress.parse(this.prima);
+            message.setRecipients(Message.RecipientType.TO, toAddresses);
+
+            // Set the subject and text
+            message.setSubject(this.predmet);
+            message.setText(this.sadrzaj);
+
+            Transport.send(message);
+
+            status = "Your message was sent.";
+        } catch (AddressException e) {
+            e.printStackTrace();
+            status = "There was an error parsing the addresses.";
+        } catch (SendFailedException e) {
+            e.printStackTrace();
+            status = "There was an error sending the message.";
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            status = "There was an unexpected error.";
+        }
+
         return "PoslanaPoruka";
     }
 
